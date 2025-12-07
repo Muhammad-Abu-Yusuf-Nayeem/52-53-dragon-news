d558054531dd05b131408e4c385b013d530b6940
assignment-01 <ykhan9463@gmail.com>
2025-12-07 21:08:46 +0600

    52-5 Implement React Fast Marquee and Moment Js on Header

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,20 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
+const LatestNews = () => {
+  return (
+    <div className="flex gap-2 items-center bg-base-200 p-2">
+      <p className="bg-[#d72050] text-base-100 px-3  py-1"> Latest</p>
+      <Marquee pauseOnHover={true} speed={50} gradient={false}>
+        <Link to="/news" className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          aliquid voluptates accusantium, non, repudiandae aspernatur atque
+          maiores tempore illo blanditiis, deleniti cum accusamus explicabo
+          quas? Maiores quibusdam est illum enim.
+        </Link>
+      </Marquee>
+    </div>
+  );
+};
+
+export default LatestNews;
--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,10 +1,14 @@
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
 
 const HomeLayout = () => {
   return (
-    <div>
+    <div className="w-11/12 mx-auto">
       <header>
         <Header></Header>
+        <section>
+          <LatestNews></LatestNews>
+        </section>
       </header>
       <nav></nav>
       <main></main>
